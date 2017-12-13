975fe53a0b234981081b13c2ae3767908d2ce8e6
Mathias Buus <mathiasbuus@gmail.com>
2017-12-13 11:33:08 +0100

    support setting identity

--- index.sh
+++ index.sh
@@ -1,5 +1,6 @@
 REMOTE_PORT=2200
 LOCAL_PORT=22
+IDENTITY=""
 
 while [ "$1" != "" ]; do
   case "$1" in
@@ -7,6 +8,8 @@ while [ "$1" != "" ]; do
     -l)            LOCAL_PORT="$2"; shift; shift;;
     --remote-port) REMOTE_PORT="$2"; shift; shift;;
     -r)            REMOTE_PORT="$2"; shift; shift ;;
+    --identity)    IDENTITY="-i $2"; shift; shift ;;
+    -i)            IDENTITY="-i $2"; shift; shift ;;
     --*)           shift; shift ;;
     *)             REMOTE="$1"; shift ;;
   esac
@@ -17,8 +20,9 @@ if [ "$REMOTE" == "" ]; then
   echo
   echo "  --remote-port, -r  [defaults to 2200]"
   echo "  --local-port,  -l  [defaults to 22]"
+  echo "  --identity,    -i  [set non default ssh identity]"
   echo
   exit 1
 fi
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=300 -o ServerAliveCountMax=2 -N -R "$REMOTE_PORT:localhost:$LOCAL_PORT" "$REMOTE"
+ssh $IDENTITY -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=300 -o ServerAliveCountMax=2 -N -R "$REMOTE_PORT:localhost:$LOCAL_PORT" "$REMOTE"
